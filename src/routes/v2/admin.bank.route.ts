15706e3890f13720d833df46d92b14f664fa1c5c
Joshua Ajorgbor <joshuaajorgbor@gmail.com>
2026-02-03 01:31:49 +0100

    feat: admin create order flow

--- src/routes/v2/admin.bank.route.ts
+++ src/routes/v2/admin.bank.route.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import bankController from "@/controllers/bank.controller.js";
+import adminAuth from "@/middlewares/admin-auth.js";
 
 const router = express.Router();
 
-router.route("/").get(bankController.getBankList);
-router.route("/validate-account").get(bankController.validateBankAccount);
+router.route("/").get(adminAuth(), bankController.getBankList);
+router
+  .route("/validate-account")
+  .get(adminAuth(), bankController.validateBankAccount);
 
 export default router;
