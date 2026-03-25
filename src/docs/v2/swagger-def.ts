58012c17129d64a6181da6caba6a92ca3621578b
Joshua Ajorgbor <joshuaajorgbor@gmail.com>
2026-03-25 12:15:28 +0100

    resolve build issues

--- src/docs/v2/swagger-def.ts
+++ src/docs/v2/swagger-def.ts
@@ -1,8 +1,11 @@
-import config from "@/config/config.js";
+import config from "../../config/config.js";
 import swaggerJsdoc from "swagger-jsdoc";
-import pkg from "../../../package.json" with { type: "json" };
-
+import fs from "fs";
 import path from "path";
+
+const pkg = JSON.parse(
+  String(fs.readFileSync(new URL("../../../package.json", import.meta.url))),
+);
 
 const { version } = pkg;
 
@@ -12,7 +15,6 @@ const options = {
     info: {
       title: "Pharmahubmedica API Documentation",
       version: version,
-      // description: 'Pharmahubmedica API Documentation',
       contact: {
         name: "Joshua Ajorgbor",
         url: "https://jajorgbor.vercel.app",
@@ -24,16 +26,9 @@ const options = {
         url: `http://localhost:${config.port}/v2`,
         description: "Local server",
       },
-      // {
-      //   url: `https://api-sandbox.pharmahubmedica.ng/v2`,
-      //   description: "Sandbox server",
-      // },
     ],
   },
-  apis: [
-    path.resolve("src/routes/**/*.ts"),
-    path.resolve("src/docs/v2/*.doc.yml"),
-  ],
+  apis: ["src/routes/**/*.ts", "src/docs/v2/*.doc.yml"],
 };
 
 export const createSwaggerSpec = () => swaggerJsdoc(options);
